67fb45b309699d551bc9b824f03bfe2acbd70ea4
Neal24luck <gosneal@gmail.com>
2021-08-10 13:19:44 -0700

    initial commit (first blow up)

---
+++ turn_combat/combatants/Opponent.gd
@@ -0,0 +1,16 @@
+extends "Combatant.gd"
+
+func set_active(value):
+	.set_active(value)
+	if not active:
+		return
+
+	$Timer.start()
+	yield($Timer, "timeout")
+	var target
+	print("Combatant.gd func set_active(value):")
+	for actor in get_parent().get_children():
+		if not actor == self:
+			target = actor
+			break
+	attack(target)
